b7e4fa8ddcac75176bcbf1ebb2966922181013ac
fullop2 <parkth32@gmail.com>
2020-09-28 20:53:32 +0900

    add ARP Header
    ARP Message의 Header를 추가
    
    Header와 관련된 함수들은 미구현

--- ComputerNetwork/src/NetworkLayer/ARPLayer.java
+++ ComputerNetwork/src/NetworkLayer/ARPLayer.java
@@ -8,6 +8,63 @@ public class ARPLayer implements BaseLayer {
 	public BaseLayer p_UnderLayer = null;
 	public ArrayList<BaseLayer> p_aUpperLayer = new ArrayList<BaseLayer>();
 
+	private class _ETHERNET_ADDR {
+		private byte[] addr = new byte[6];
+
+		public _ETHERNET_ADDR() {
+			this.addr[0] = (byte) 0x00;
+			this.addr[1] = (byte) 0x00;
+			this.addr[2] = (byte) 0x00;
+			this.addr[3] = (byte) 0x00;
+			this.addr[4] = (byte) 0x00;
+			this.addr[5] = (byte) 0x00;
+		}
+	}
+	
+	private class _IP_ADDR {
+		private byte[] addr = new byte[4];
+
+		public _IP_ADDR() {
+			this.addr[0] = (byte) 0x00;
+			this.addr[1] = (byte) 0x00;
+			this.addr[2] = (byte) 0x00;
+			this.addr[3] = (byte) 0x00;
+		}
+	}
+	
+	@SuppressWarnings("unused")
+	private class _ARP_HEADER {
+		byte[] hardwareType;
+		byte[] protocolType;		
+		byte hardwareSize;
+		byte protocolSize;
+		byte[] opcode;
+		_ETHERNET_ADDR enetSenderAddr;
+		_IP_ADDR ipSenderAddr;
+		_ETHERNET_ADDR enetTargetAddr;
+		_IP_ADDR ipTargetAddr;
+		
+		final int headerSize = 28;
+		
+		public _ARP_HEADER() {
+			hardwareType = new byte[2];
+			hardwareType[0] = 0x00; hardwareType[1] = 0x01;
+			protocolType = new byte[2];
+			protocolType[0] = 0x08; protocolType[1] = 0x00;
+			hardwareSize = 0x06;
+			protocolSize = 0x04;
+			opcode = new byte[2];
+			opcode[0] = 0x00; opcode[1] = 0x01;
+			enetSenderAddr = new _ETHERNET_ADDR();
+			ipSenderAddr = new _IP_ADDR();
+			enetTargetAddr = new _ETHERNET_ADDR();
+			ipTargetAddr = new _IP_ADDR();
+		}
+	}
+
+	_ARP_HEADER m_sHeader = new _ARP_HEADER();
+	
+	
 	@Override
 	public void SetUnderLayer(BaseLayer pUnderLayer) {
 		// TODO Auto-generated method stub
