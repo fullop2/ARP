22eb61c93973020a1f0e9a9686cb416fb44a7044
fullop2 <parkth32@gmail.com>
2020-10-08 13:57:41 +0900

    add arp cache send
    ARP Cache Panel에서 send를 누르면 ip가 테이블에 등록
    뷰에 보이도록 함

--- ComputerNetwork/src/EventHandlers/ARPTableEventHandlers.java
+++ ComputerNetwork/src/EventHandlers/ARPTableEventHandlers.java
@@ -1,14 +1,33 @@
 package EventHandlers;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import NetworkLayer.ARPLayer;
 import NetworkLayer.LayerManager;
-import View.AppView;
+import View.ARPCachePanel;
 
 public class ARPTableEventHandlers implements EventHandlers {
 
 	@Override
 	public void setEventHandlers(LayerManager layerManager) {
-		// TODO Auto-generated method stub
-		
+		ARPCachePanel.btnArpSend.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				String ip = ARPCachePanel.ArpIP.getText();
+				String[] ipSplit = ip.split("\\.");
+				byte[] ipAddress = new byte[4];
+				for (int i = 0; i < 4; i++)
+					ipAddress[i] = (byte) (Integer.parseInt(ipSplit[i], 16) & 0xff);
+				
+				ARPLayer arpLayer = ((ARPLayer)layerManager.GetLayer("ARP"));
+				arpLayer.addARPCache(ipAddress, null);
+				
+				ARPCachePanel.ArpTable.append(ip+"\t????????????\tincompleted\n");
+				
+			}
+		});
 	}
 
 
