a85361097b97b2f4262cf7960f9f146d43c2a2e0
fullop2 <parkth32@gmail.com>
2020-10-10 11:17:05 +0900

    add ARP App Layer
    ARP 통신을 위한 최상단 레이어 추가

---
+++ ComputerNetwork/src/NetworkLayer/ARPAppLayer.java
@@ -0,0 +1,55 @@
+package NetworkLayer;
+
+import java.util.ArrayList;
+
+public class ARPAppLayer implements BaseLayer {
+	public int nUpperLayerCount = 0;
+	public String pLayerName = null;
+	public BaseLayer p_UnderLayer = null;
+	public ArrayList<BaseLayer> p_aUpperLayer = new ArrayList<BaseLayer>();
+
+	
+	public ARPAppLayer(String pName) {
+		pLayerName = pName;
+	}
+	
+	@Override
+	public void SetUnderLayer(BaseLayer pUnderLayer) {
+		if (pUnderLayer == null)
+			return;
+		this.p_UnderLayer = pUnderLayer;
+	}
+
+	@Override
+	public void SetUpperLayer(BaseLayer pUpperLayer) {
+		if (pUpperLayer == null)
+			return;
+		this.p_aUpperLayer.add(nUpperLayerCount++, pUpperLayer);
+	}
+
+	@Override
+	public String GetLayerName() {
+		return pLayerName;
+	}
+
+	@Override
+	public BaseLayer GetUnderLayer() {
+		if (p_UnderLayer == null)
+			return null;
+		return p_UnderLayer;
+	}
+
+	@Override
+	public BaseLayer GetUpperLayer(int nindex) {
+		if (nindex < 0 || nindex > nUpperLayerCount || nUpperLayerCount < 0)
+			return null;
+		return p_aUpperLayer.get(nindex);
+	}
+
+	@Override
+	public void SetUpperUnderLayer(BaseLayer pUULayer) {
+		this.SetUpperLayer(pUULayer);
+		pUULayer.SetUnderLayer(this);
+
+	}
+}
