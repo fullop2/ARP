d8059f7f1f6c669e2ac993f494a1415d02d1d435
Park Taehyun <parkth32@gmail.com>
2020-10-13 12:54:38 +0900

    add dst addr setting
    목적지 주소를 설정하는 view 추가

--- ComputerNetwork/src/View/AppView.java
+++ ComputerNetwork/src/View/AppView.java
@@ -1,14 +1,6 @@
 package View;
 
-import java.awt.Container;
-import java.awt.FileDialog;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.swing.*;
-
-import org.jnetpcap.Pcap;
-import org.jnetpcap.PcapIf;
 
 public class AppView extends JFrame{
 	public AppView() {
@@ -19,7 +11,7 @@ public class AppView extends JFrame{
 
 		setTitle("ARP");
 
-		setBounds(250, 250, 1220, 340);
+		setBounds(250, 250, 1220, 345);
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -28,7 +20,7 @@ public class AppView extends JFrame{
 		add(new ARPCachePanel(0,0));
 		add(new ProxyARPPanel(400, 0));
 		add(new GARPPanel(800,0));
-		add(new AddressPanel(800,150));
+		add(new AddressPanel(800,70));
 		
 		setVisible(visible);
 	}
