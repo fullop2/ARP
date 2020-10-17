048bd09b10b80897485d582e76960c9af2187b79
Park Taehyun <taehyun.park.fullop@gmail.com>
2020-10-17 12:49:05 +0900

    Bug fix/arp layer (#25)
    * refactor ARP Request Target
    
    ARP Request에서 Ethernet Target을
    
    ARP Reply 조건을
    1) 나에게 온 경우
    2) 브로드캐스팅(Ethernet=0)이고 내 프록시 테이블에 존재하는 경우
    3) 브로드캐스팅이고 IP Sender와 Target이 같은 경우 (GARP)
    로 변경
    
    0xff에서 0x00으로 변경
    
    ARP 생존 시간을
    미등록시 3분
    등록시 20분으로 변경
    
    * refactor ARP Timer
    
    ARP Timer를 실제 시스템이 지난 시간 만큼 감소하도록 변경
    
    Co-authored-by: Park Taehyun <parkth32@gmail.com>

--- ComputerNetwork/src/NetworkLayer/ARPLayer.java
+++ ComputerNetwork/src/NetworkLayer/ARPLayer.java
@@ -34,9 +34,9 @@ public class ARPLayer implements BaseLayer {
 			setIp(ip);
 			
 			if(isNIL(ethernet))
-				setTimeToLive(30000);
+				setTimeToLive(180000);
 			else
-				setTimeToLive(60000);
+				setTimeToLive(1200000);
 		}
 		
 		public void setTimeToLive(int milliSecond) {
@@ -213,27 +213,38 @@ public class ARPLayer implements BaseLayer {
 		return true;
 	}
 	
-	private boolean isBroadCast(byte[] ethernetAddr) {
-		return Arrays.equals(BROADCAST_ETHERNET, ethernetAddr);
-	}
-	
 	private boolean isNIL(byte[] ethernetAddr){
 		return Arrays.equals(NIL_ETHERNET, ethernetAddr);
 	}
 	
+	private boolean isGARP(_ARP_HEADER header){
+		return Arrays.equals(header.ipSenderAddr.addr, header.ipTargetAddr.addr) && isNIL(header.enetTargetAddr.addr);
+	}
+	
+	private boolean needProxy(_ARP_HEADER header){
+		return hasIPInProxyTable(header.ipTargetAddr.addr) && isNIL(header.enetTargetAddr.addr);
+	}
 	
 	class ARPTimer extends Thread {
 		
+		private long beforeTime;
+		
+		ARPTimer(){
+			beforeTime = System.currentTimeMillis();
+		}
 		@Override
 		public void run() {
 			while(true) {
 				try {
 					Thread.sleep(1000);
 					
+					long timeElipse = System.currentTimeMillis() - beforeTime;
+					beforeTime =  System.currentTimeMillis();
+					
 					Iterator<ARPCache> iter = arpCacheTable.iterator();
 					while(iter.hasNext()) {
 						ARPCache cache = iter.next();
-						cache.timeToLive -= 1000;
+						cache.timeToLive -= timeElipse;
 						if(cache.timeToLive <= 0) {
 							iter.remove();
 							updateARPCachePanel();
@@ -288,15 +299,14 @@ public class ARPLayer implements BaseLayer {
 		 * 1. ARP 요청이고
 		 * 
 		 * 1) 나에게 온 요청일 경우
-		 * 2) 브로드캐스트이고 목적지가 내 프록시 테이블에 있을 경우 
-		 * 3) GARP라서 Ethernet Target이 0일 경우 
+		 * 2) 프록싱 가능할 경우
+		 * 3) GARP인 경우
 		 * 
 		 * 내 맥을 넣어서 답장
 		 */
 		if(isRequest(receivedHeader.opcode) && 
-			(isNIL(receivedHeader.enetTargetAddr.addr) ||
-			 isMine(receivedHeader.ipTargetAddr.addr) || 
-			 (isBroadCast(receivedHeader.enetTargetAddr.addr) && hasIPInProxyTable(receivedHeader.ipTargetAddr.addr)))) 
+				(isMine(receivedHeader.ipTargetAddr.addr) || needProxy(receivedHeader)|| isGARP(receivedHeader))
+		   )
 		{	
 			receivedHeader.opcode[1] = 0x02; // make reply
 			receivedHeader.enetTargetAddr = arpHeader.enetSenderAddr;
