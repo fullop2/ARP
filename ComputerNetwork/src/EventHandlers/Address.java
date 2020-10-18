1e37f1cc5d78785383f06f53df2878c2181ae985
Park Taehyun <taehyun.park.fullop@gmail.com>
2020-10-18 14:49:03 +0900

    Feature/view (#33)
    * add address 처리 단일화
    
    ip, mac address 인식 및 결과 반환을 위한 공통 함수 추가
    
    * refactor Address
    
    Address 접근 방법 단일화
    
    * refactor Chat Address
    
    chat Address 접근 변경
    
    * merge master
    
    * refactor Address Information
    
    Address 정보를 모두 Address로 이동
    정적 전역 변수로 두어 GUI에서 일관된 값을 사용할 수 있게 설정
    
    * refactor View
    
    GARP Address 설정 변경
    
    View에 보이는 MAC IP 수정
    
    Co-authored-by: Park Taehyun <parkth32@gmail.com>

--- ComputerNetwork/src/EventHandlers/Address.java
+++ ComputerNetwork/src/EventHandlers/Address.java
@@ -1,9 +1,22 @@
 package EventHandlers;
 
+import java.util.Arrays;
+
 import javax.swing.JOptionPane;
 
 public class Address {
-	public static byte[] mac(String stringMacAddress) {
+	
+	protected static final byte[] ETH_NIL = { (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00};
+	protected static final byte[] ETH_BROADCAST = { (byte)0xff, (byte)0xff, (byte)0xff, (byte)0xff, (byte)0xff, (byte)0xff};
+	protected static final byte[] ETH_TYPE_ARP = { (byte) 0x08, (byte)0x06 };
+	protected static final byte[] ETH_TYPE_IP = { (byte) 0x08, (byte)0x00 };
+	protected static final byte[] APP_PORT_CHAT = { (byte) 0x20, (byte)0x10 };
+	
+	protected static boolean isNIL(byte[] eth) {
+		return Arrays.equals(ETH_NIL, eth);
+	}
+	
+	protected static byte[] mac(String stringMacAddress) {
 		stringMacAddress.trim();
 		if(!stringMacAddress.matches("([0-9A-F]{2}[:-]){5}([0-9A-F]{2})")) {
 			JOptionPane.showMessageDialog(null, "[ERR] MAC을 제대로 설정해주세요");
@@ -17,18 +30,21 @@ public class Address {
 		return address;
 	}
 	
-	public static byte[] ip(String stringIpAddress) {
+	protected static byte[] ip(String stringIpAddress) {
 		stringIpAddress.trim();
 		if(!stringIpAddress.matches("((2[0-5]|1[0-9]|[0-9])?[0-9]\\.){3}((2[0-5]|1[0-9]|[0-9])?[0-9])")) {
 			JOptionPane.showMessageDialog(null, "[ERR] IP를 제대로 설정해주세요");
 			return null;
 		}
 		
+
 		String[] ipSplit = stringIpAddress.split("\\.");
-		byte[] address = new byte[6];
+		byte[] address = new byte[4];
 		for(int i = 0; i < 4; i++) {
 			address[i] = (byte) (Integer.parseInt(ipSplit[i]) & 0xff);
 		}
 		return address;
 	}
+
+	
 }
--- ComputerNetwork/src/EventHandlers/GARPEventHandlers.java
+++ ComputerNetwork/src/EventHandlers/GARPEventHandlers.java
@@ -27,46 +27,26 @@ public class GARPEventHandlers implements EventHandlers {
 				 * 목적지 브로드캐스팅
 				 * 타입 ARP로 설정
 				 */
-				
-				String mac = GARPPanel.GARPMacAddr.getText();	
-				if(!macValidation(mac)) {
-					JOptionPane.showMessageDialog(null, "[ERR] MAC을 제대로 설정해주세요");
-					return;
-				}
-				
-				String[] macSplit = mac.split("-");
-				byte[] macAddress = new byte[6];				
-				for(int i = 0; i < 6; i++)
-					macAddress[i] = (byte) (Integer.parseInt(macSplit[i],16) & 0xff);
+				byte[] macAddress = Address.mac(GARPPanel.GARPMacAddr.getText());
+				if(macAddress == null) return;
 				
 				EthernetLayer ethernetLayer = ((EthernetLayer)layerManager.GetLayer("Ethernet"));
-				byte[] nil = { (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00};
-				byte[] broadcast = { (byte)0xff, (byte)0xff, (byte)0xff, (byte)0xff, (byte)0xff, (byte)0xff};
-				byte[] ethType = { (byte) 0x08, (byte)0x06 };
 				
-				ethernetLayer.setDstEthernetAddress(broadcast);
-				ethernetLayer.setEthernetType(ethType);
+				ethernetLayer.setDstEthernetAddress(Address.ETH_BROADCAST);
+				ethernetLayer.setEthernetType(Address.ETH_TYPE_ARP);
 				
-				String ip = AddressPanel.srcIPAddress.getText();
-				StringTokenizer ipSplit = new StringTokenizer(ip, ".");
-				byte[] ipAddress = new byte[4];
-				for (int i = 0; i < 4; i++)
-					ipAddress[i] = (byte)Integer.parseInt(ipSplit.nextToken());
+				byte[] ipAddress = Address.ip( AddressPanel.srcIPAddress.getText());
+				if(ipAddress == null) return;
 				
 				ARPLayer arpLayer = ((ARPLayer)layerManager.GetLayer("ARP"));
 				arpLayer.setIPTargetAddress(ipAddress);
 				arpLayer.setEthernetSenderAddress(macAddress);
-				arpLayer.setEthernetTargetAddress(nil);
+				arpLayer.setEthernetTargetAddress(Address.ETH_NIL);
 				// no info for tcp,ip
 				
 				// app
 				ARPAppLayer arpAppLayer = ((ARPAppLayer)layerManager.GetLayer("ARPA"));			
 				arpAppLayer.Send(null,0);
-			}
-			
-			boolean macValidation(String macAddress) {
-				
-				return macAddress.matches("([0-9A-F]{2}[:-]){5}([0-9A-F]{2})");
 			}
 		});
 	}
